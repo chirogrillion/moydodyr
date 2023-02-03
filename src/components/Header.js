0f354e902be546bd11b74172f95c8b06f50b4c28
krgl <kjiirugriilian@gmail.com>
2023-02-03 16:48:19 +0300

    23-02-03-1

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,17 +13,16 @@ function Header() {
           <img src={logo}/>
         </section>
         <section className="Header-contacts">
-          <a className="Header-user_location">Минск</a>
+          <a className="link-location">Минск</a>
           <div className="Header-call_center">
-            <span>Колл-центр</span>
-            <a>656-32-32</a>
+            <a className="link-tel">656-32-32</a>
             <span>с 9:00 до 21:00</span>
           </div>
           <ul className="Header-consumer_info">
-            <li><a>Пункты выдачи</a></li>
-            <li><a>Доставка</a></li>
-            <li><a>Оплата</a></li>
-            <li><a>Акции</a></li>
+            <li><a className="link">Пункты выдачи</a></li>
+            <li><a className="link">Доставка</a></li>
+            <li><a className="link">Оплата</a></li>
+            <li><a className="link">Акции</a></li>
           </ul>
         </section>
         <SearchBar/>
@@ -32,7 +31,6 @@ function Header() {
           <li><a>Категория 1</a></li>
           <li><a>Категория 2</a></li>
           <li><a>Категория 3</a></li>
-          <li><a>Категория 4</a></li>
           <li><a>Ещё</a></li>
         </ul>
         <section className="Header-user">
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
+
+  const inputRef = useRef(null);
+  const focusOnInput = () => inputRef.current.focus();
+
   return (
-    <input
-      className="SearchBar"
-      type="text"
-      placeholder="Поиск товаров"
-    />
+    <section className="SearchBar">
+      <div
+        className="SearchBar-container"
+        onClick={focusOnInput}
+      >
+        <span>&#61442;</span>
+        <input
+          type="text"
+          placeholder="Поиск товаров"
+          ref={inputRef}
+        />
+      </div>
+    </section>
   );
+
 };
 
 export default SearchBar;
