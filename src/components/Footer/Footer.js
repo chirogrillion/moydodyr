efc7ad6ce979be87354106dd1bcceb696ecc24d3
kjiirugriilian <kjiirugriilian@gmail.com>
2023-02-16 19:37:02 +0300

    23-02-16-5

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './Footer.css';
 
+const logo = require('../../assets/logo/moydodyr.png');
+
 const Footer = () => {
   return (
-    <footer className="Footer"></footer>
+    <footer className="Footer">
+      <div className="Footer-container">
+        <p>&copy; 2023 ООО &laquo;Чистый дом&raquo;</p>
+        <img src={logo} alt="Мойдодыр"/>
+        <p>Разработка сайта: Стёпин Сергей</p>
+      </div>
+    </footer>
   );
 };
 
